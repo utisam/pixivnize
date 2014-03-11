31ad3cf6bdca41170493eb72693f706728c17309
Masatoshi Tsushima <utisam@gmail.com>
2014-03-11 11:23:21 +0900

    issue #2 現在のmainをsample.cppに移動
    main.cppのmainにサーバのコードを書く

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -7,27 +7,6 @@ using namespace std;
 using namespace pma;
 
 int main(int argc, char* argv[]) {
-	if (argc != 2) {
-		cout << "Usage: " << argv[0] << " [tags]" << endl;
-	}
-	ifstream tags(argv[1]);
-	vector<string> ptns;
-	while (tags) {
-		string str;
-		getline(tags, str);
-		auto pos = str.find('\t');
-		string tag = str.substr(0, pos);
-		ptns.push_back(tag);
-	}
-	PMA* pma = build(ptns);
-	cout << "complete!" << endl;
-
-	string msg;
-	while (cout << "message: ", cin >> msg) {
-		for (auto m : pma->match(msg)) {
-			cout << m << '\t' << m.matchstr(msg) << endl;
-		}
-	}
 	return 0;
 }
 
