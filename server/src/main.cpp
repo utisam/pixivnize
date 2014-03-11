f1af03c9b3888cecb0b1eacb4d1b5ad317cb9f58
Masatoshi Tsushima <utisam@gmail.com>
2014-03-11 12:21:20 +0900

    issue #2 サーバのサンプルを追加

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,12 +1,25 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
-#include "pma.h"
-using namespace std;
-using namespace pma;
+#include <boost/network/protocol/http/server.hpp>
+namespace http = boost::network::http;
 
-int main(int argc, char* argv[]) {
-	return 0;
+class handler;
+
+using http_server = http::server<handler>;
+
+class handler {
+public:
+	void operator() (http_server::request const &request,
+					 http_server::response &response) {
+		response = http_server::response::stock_reply(
+			http_server::response::ok, "Hello, world!");
+	}
+	void log(http_server::string_type const &info) {
+		std::cerr << "ERROR: " << info << '\n';
+	}
+};
+
+int main(int arg, char * argv[]) {
+	handler handler_;
+	http_server::options options(handler_);
+	http_server server_(options.address("0.0.0.0").port("8000"));
+	server_.run();
 }
-
