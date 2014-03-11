9a76a23b774ebc323fb6da15b5a4c553a4eb3e7c
Masatoshi Tsushima <utisam@gmail.com>
2014-03-11 19:34:11 +0900

    issue #2 メソッドとサイズを見てBad Requestを投げるようにした

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,8 +1,10 @@
 #include <signal.h>
 #include <unistd.h>
+#include <array>
 #include <iostream>
 #include <boost/network/include/http/server.hpp>
 #include <boost/network/utils/thread_pool.hpp>
+using namespace std;
 namespace http = boost::network::http;
 
 class handler_type;
@@ -11,26 +13,51 @@ using http_server = http::async_server<handler_type>;
 
 class handler_type {
 public:
+	static void bad_request(http_server::connection_ptr connection) {
+		connection->set_status(http_server::connection::bad_request);
+		array<http_server::response_header, 2> headers = {{
+			{"Connection", "close"},
+			{"Content-Type", "Content-Type: text/html; charset=utf-8"},
+		}};
+		connection->write("Bad Request");
+	}
 	static void read_body(boost::iterator_range<char const *> input_range,
 				boost::system::error_code error_code, size_t size,
 				http_server::connection_ptr connection) {
+		cout << size << endl;
+		if (size <= (1 << 20)) {
+			connection->set_status(http_server::connection::accepted);
+			array<http_server::response_header, 2> headers = {{
+				{"Connection", "close"},
+				{"Content-Type", "Content-Type: application/json; charset=utf-8"},
+			}};
+			connection->set_headers(headers);
+			for (char c : input_range) {
+				cout << c;
+			}
+			cout << endl;
+			connection->write("[\"Hello, world!\"]");
+		}
+		else {
+			bad_request(connection);
+		}
 	}
 	void operator()(http_server::request const& request,
 					http_server::connection_ptr connection) {
-		connection->read(handler_type::read_body);
-		std::string ip = source(request);
-		std::cout << "[" << ip << "]: " << static_cast<std::string>(http::destination(request)) << std::endl;
+		string ip = source(request);
+		string dest = static_cast<string>(http::destination(request));
+		cout << "[" << ip << "]: " << dest << endl;
 
-		http_server::response_header headers[] = {
-			{"Connection","close"},
-			{"Content-Type", "text/html"},
-		};
-		connection->set_headers(boost::make_iterator_range(headers, headers + 2));
-		connection->set_status(http_server::connection::accepted);
-		connection->write("Hello, world!");
+		if (request.method == "POST") {
+			connection->read(handler_type::read_body);
+		}
+		else {
+			bad_request(connection);
+		}
 	}
 };
 
+const int THREAD_SIZE = 2;
 static http_server* server_ptr;
 static void signal_handler(int sig) {
 	server_ptr->stop();
@@ -39,18 +66,20 @@ static void signal_handler(int sig) {
 int main(int arg, char * argv[]) {
 	handler_type handler;
 	http_server::options options(handler);
-	options.thread_pool(boost::make_shared<boost::network::utils::thread_pool>(2));
+	auto pool = boost::make_shared<boost::network::utils::thread_pool>(THREAD_SIZE);
+	options.thread_pool(pool);
 	http_server server(options.address("0.0.0.0").port("8000"));
 	server_ptr = &server;
 	
-	boost::thread t1(boost::bind(&http_server::run, &server));
-	boost::thread t2(boost::bind(&http_server::run, &server));
+	array<boost::thread, THREAD_SIZE> threads = {{
+		boost::thread(boost::bind(&http_server::run, &server)),
+		boost::thread(boost::bind(&http_server::run, &server)),
+	}};
 	if (signal(SIGINT, signal_handler) == SIG_ERR)
 		return -1;
-	std::cout << "start server" << std::endl;
+	cout << "starting server" << endl;
 	server.run();
-	t1.join();
-	t2.join();
-	std::cout << "server has exited" << std::endl;
+	for (auto& t : threads) t.join();
+	cout << "server has exited" << endl;
 	return 0;
 }
